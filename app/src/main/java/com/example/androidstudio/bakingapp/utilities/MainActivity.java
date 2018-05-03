221e558ba400b4f3493143254fa119868ed9bb98
marcotf-git <marcotf.dev@gmail.com>
2018-05-03 16:32:45 -0300

    initial commit - recipes database contract

---
+++ app/src/main/java/com/example/androidstudio/bakingapp/utilities/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.androidstudio.bakingapp.utilities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.androidstudio.bakingapp.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
